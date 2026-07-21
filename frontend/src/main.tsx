f73bccc3d667d512b028c41ae184fffd312e2c60
aaronzizi <elonemacc@gmail.com>
2026-07-21 16:27:30 +0200

    add toaster in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from '@/components/ui/sonner';
 import App from './App.tsx'
 
 const queryClient = new QueryClient()
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster position="bottom-right" richColors />
     </QueryClientProvider>
   </StrictMode>,
 )
